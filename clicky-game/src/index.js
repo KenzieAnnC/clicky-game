52740235250905144bcb38307a129d857377ea3d
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-07-27 16:56:13 -0500

    deplpying

--- clicky-game/src/index.js
+++ clicky-game/src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var PORT = process.env.PORT || 3000;
-
-app.listen(PORT, function () {
-    console.log("app listening on " + port)
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
